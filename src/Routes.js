c9acb03b06ea56ddff81b0de0c595771f59899a6
vamandeshmukh <vamandeshmukh@gmail.com>
2022-05-18 16:51:57 +0530

    react routing

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,49 @@
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+
+import EmployeeData from './components/EmployeeData';
 
 const Routes = () => {
 
     return (
         <div>
-            <p>Routes component</p>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <div>
+                        <Switch>
+                            <Route path="/home"> <Home/> </Route>
+                            <Route path="/emp"> <EmployeeData/> </Route>
+                            <Route path="/login"> <Login/> </Route>
+                            <Route path="/register"> <Register/> </Route>
+                        </Switch>
+                    </div>
+                </div>
+            </BrowserRouter>
         </div>
     );
-
 }
 
 export default Routes;
+
+
+
+
+
+// const Routes = () => {
+
+//     return (
+//         <div>
+//             <p>Routes component</p>
+//         </div>
+//     );
+
+// }
+
+// export default Routes;
